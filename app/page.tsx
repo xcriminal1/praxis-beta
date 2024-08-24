01f9a3af73e809723d9c77a6005e433e26362aad
gourab8389 <deyg6988@gmail.com>
2024-08-24 15:40:45 +0530

    chore: Update page layout and styles for better responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import BoxRevealDemo from "@/components/magicui/box-reveal-component";
 
 export default function Home() {
   return (
-    <main className="px-10 pt-3 md:pt-10 flex flex-col justify-center bg-red-400">
-      <div className="h-full md:min-h-[70vh] flex w-full flex-col md:flex-row items-center justify-between gap-18 bg-blue-600">
-        <div className="w-full md:w-[65%] h-full md:h-[75vh] md:pl-8 px-2 pt-6">
+    <main className="px-10 flex flex-col justify-center items-center py-10">
+      <div className="h-full md:min-h-[70vh] flex w-full flex-col md:flex-row items-center justify-center gap-16">
+        <div className="w-full md:w-[65%] h-full md:h-[75vh] flex items-center">
           <BoxRevealDemo />
         </div>
-        <div className="w-full md:w-[60%] h-full md:h-[60vh] items-center justify-center pt-6 hidden md:flex">
+        <div className="w-full md:w-[60%] h-full md:h-[60vh] items-center justify-center flex">
           <SortableListDemo />
         </div>
       </div>
