2890d54e34a201f3d3109f9bf607eef77f439ebf
Suman Mondal <mondalsumanrock20@gmail.com>
2024-08-24 14:08:07 +0530

    chore: Update auth layout to use responsive height

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex items-center justify-center min-h-screen h-full w-full">
+    <div className="w-full h-[90vh] flex items-center justify-center">
       {children}
     </div>
   );
