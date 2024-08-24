3d43686a5ad28a5a84c935862bbffb4f5eb50911
Suman Mondal <mondalsumanrock20@gmail.com>
2024-08-24 11:40:44 +0530

    chore: Add utility function for merging CSS classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(GeistSans.variable, "font-sans overflow-x-hidden")}>
+        {children}
+      </body>
     </html>
   );
 }
